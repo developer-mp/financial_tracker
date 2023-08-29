14000c3b9c15b8f1ad17c3dd4e808730655900cb
Mikita Piastou <piastoumikita@gmail.com>
2023-08-28 21:08:46 -0600

    Temp changes

---
+++ TotalExpenses.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+
+namespace FinancialTracker
+{
+    public class TotalExpenses
+    {
+        private ObservableCollection<ExpenseItem> _expenseList;
+
+        public TotalExpenses(ObservableCollection<ExpenseItem> expenseList)
+        {
+            _expenseList = expenseList;
+        }
+
+        public double CalculateTotalExpenses()
+        {
+            return _expenseList.Sum(expense => expense.Amount);
+        }
+    }
+}
