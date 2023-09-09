836e885fff8f48205db4cbc98c4f8d11b5d438ba
Mikita Piastou <piastoumikita@gmail.com>
2023-09-09 17:07:57 -0600

    Added pdf report functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Drawing;
+using System.IO;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using FinancialTracker.Service;
 using FinancialTracker.Utils;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.Win32;
 
 namespace FinancialTracker
 {
@@ -94,6 +100,73 @@ namespace FinancialTracker
 
         private void PrintButtonClick(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "PDF Files|*.pdf";
+                saveFileDialog.Title = "Save PDF Report";
+                saveFileDialog.FileName = "SummaryReport.pdf";
+
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    string pdfFilePath = saveFileDialog.FileName;
+
+                    GenerateAndSavePDF(pdfFilePath);
+
+                    MessageBox.Show("PDF report saved successfully", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error saving PDF report: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void GenerateAndSavePDF(string pdfFilePath)
+        {
+            try
+            {
+                Document doc = new Document();
+                PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream(pdfFilePath, FileMode.Create));
+                doc.Open();
+
+                PdfPTable table = new PdfPTable(1);
+                table.WidthPercentage = 100;
+
+                PdfPCell cell = new PdfPCell(new Phrase($"Total Expenses: ${TotalExpensesTextBlock.Text}"));
+                cell.Border = iTextSharp.text.Rectangle.NO_BORDER;
+                cell.HorizontalAlignment = Element.ALIGN_CENTER;
+                cell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                cell.FixedHeight = 50f;
+                table.AddCell(cell);
+
+                PdfPCell chartCell = new PdfPCell();
+                BitmapImage chartImage = (BitmapImage)ChartImage.Source;
+                var encoder = new JpegBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(chartImage));
+
+                string tempImagePath = Path.GetTempFileName() + ".jpg";
+
+                using (var stream = new FileStream(tempImagePath, FileMode.Create))
+                {
+                    encoder.Save(stream);
+                }
+
+                iTextSharp.text.Image image = iTextSharp.text.Image.GetInstance(tempImagePath);
+                image.ScaleAbsolute(500f, 400f);
+                chartCell.AddElement(image);
+                table.AddCell(chartCell);
+
+                doc.Add(table);
+                File.Delete(tempImagePath);
+                doc.Close();
+
+                MessageBox.Show("PDF report generated successfully", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error generating PDF report: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void GenerateChart()
